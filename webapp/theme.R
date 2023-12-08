af98dee601a6c367e15fbd57700013c8ccc58060
alejandro <aleabraham6@gmail.com>
2023-12-08 23:17:40 +0100

    try

--- webapp/theme.R
+++ webapp/theme.R
@@ -1,26 +1,26 @@
 
 # Global Theme ================================================================================
-
-zerotoonec_theme =
-   bs_theme(
-      bootswatch = "litera",
-      primary = '#75A5B7',
-      secondary = '#8DC2C9',
-      info = '#4C6279',
-      danger = '#F29191',
-      warning = '#f37736',
-      success = '#6EBDAB',
-      "font-size-base" = "0.85rem",
-      'navbar-bg' = '#75A5B7',
-      'navbar-ligh-margin' = '50px',
-      'navbar-light-bg' = '#4C6279',
-      'navbar-light-color' = '#f2f2f2',
-      'navbar-light-active-color' = '#fff',
-      'navbar-light-hover-color' = '#8DC2C9',      
-      'body-bg' = '#f9f9f9',
-      'body-color' = '#6c757d',
-      'heading-color' = '#6c757d'
-   )
+# 
+# zerotoonec_theme =
+#    bs_theme(
+#       bootswatch = "litera",
+#       primary = '#75A5B7',
+#       secondary = '#8DC2C9',
+#       info = '#4C6279',
+#       danger = '#F29191',
+#       warning = '#f37736',
+#       success = '#6EBDAB',
+#       "font-size-base" = "0.85rem",
+#       'navbar-bg' = '#75A5B7',
+#       'navbar-ligh-margin' = '50px',
+#       'navbar-light-bg' = '#4C6279',
+#       'navbar-light-color' = '#f2f2f2',
+#       'navbar-light-active-color' = '#fff',
+#       'navbar-light-hover-color' = '#8DC2C9',      
+#       'body-bg' = '#f9f9f9',
+#       'body-color' = '#6c757d',
+#       'heading-color' = '#6c757d'
+#    )
 
 
 
