7c1e89b046fe920e73b2e868ac96890d0f85169f
alejandro <aleabraham6@gmail.com>
2023-12-09 00:21:07 +0100

    sss

--- webapp/setup.R
+++ webapp/setup.R
@@ -27,20 +27,20 @@ cat('021 ADMP - LOADING: Please wait while app is getting ready in your default 
 
 
 ## Euronext Growth Milan List ----------------------------------------------------------------
-
-### Load 
-engm_equities_list = setDT(read.csv2(file.path('data', 'Euronext_Equities_2023-12-06.csv')))
-
-### Clean & Create
-engm_equities_list[, id := .I]
-engm_equities_list[, ticker_code := paste0(Symbol, '.MI')]
-engm_equities_list = engm_equities_list[-c(1:3), .(id, Market, ticker_code, Symbol, Name, ISIN, last.Trade.MIC.Time)]
-names(engm_equities_list) = c('id', 'name_market', 'code_ticker', 'code_symbol', 'name_company', 'code_isin', 'date_lastTrade')
-
-engm_equities_list[, date_lastTrade := as.Date(date_lastTrade, format = "%d/%m/%Y %H:%M")]
-engm_equities_list[, flag_active := fifelse(date_lastTrade > (Sys.Date() - 30), TRUE, FALSE)]
-
-### Export
-write.csv2(engm_equities_list, file.path('data', 'engm_equities_list.csv')) 
-
-
+# 
+# ### Load 
+# engm_equities_list = setDT(read.csv2(file.path('data', 'Euronext_Equities_2023-12-06.csv')))
+# 
+# ### Clean & Create
+# engm_equities_list[, id := .I]
+# engm_equities_list[, ticker_code := paste0(Symbol, '.MI')]
+# engm_equities_list = engm_equities_list[-c(1:3), .(id, Market, ticker_code, Symbol, Name, ISIN, last.Trade.MIC.Time)]
+# names(engm_equities_list) = c('id', 'name_market', 'code_ticker', 'code_symbol', 'name_company', 'code_isin', 'date_lastTrade')
+# 
+# engm_equities_list[, date_lastTrade := as.Date(date_lastTrade, format = "%d/%m/%Y %H:%M")]
+# engm_equities_list[, flag_active := fifelse(date_lastTrade > (Sys.Date() - 30), TRUE, FALSE)]
+# 
+# ### Export
+# write.csv2(engm_equities_list, file.path('data', 'engm_equities_list.csv')) 
+# 
+# 
